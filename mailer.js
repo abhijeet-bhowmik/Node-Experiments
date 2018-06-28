61942e38badbfc8b314c62c4a3a9508c7157373a
abhijeet-newput <=>
2018-06-28 16:13:07 +0530

    Added CRON

--- mailer.js
+++ mailer.js
@@ -5,6 +5,7 @@ const kue = require('kue');
 const queue = kue.createQueue();
 const kueUiExpress = require('kue-ui-express');
 kueUiExpress(app, '/kue/', '/kue-api/');
+const cron = require('node-cron');
 
 
 
@@ -23,8 +24,8 @@ const mailOptions = {
   subject : 'Test mail',
   html : '<p>Hello. This is a test mail.</p>'
 };
-
-//function that sends mail.
+//
+// //function that sends mail.
 var mailing_func = function(){
 
   return new Promise(function(resolve, reject){
@@ -34,7 +35,7 @@ var mailing_func = function(){
     })
   })
 }
-//reciever lists
+// //reciever lists
 var mailing_list =  ["abhijeetbhowmik@outlook.com", "rachna281996@gmail.com", "abhi.bh.31296@gmail.com"];
 
 
@@ -44,17 +45,21 @@ var mailman = function(){
       var job = queue.create(`'email${i}'`, {
         title : "Email Sending",
         to : `"${mailing_list[i]}"`
-      }).priority('high').attempts(5).save();
+      }).attempts(5).ttl(100000).priority('high').save();
       job.on('complete', function(result){
         console.log(result);
       });
+      job.on('progress', function(progress, data){
+        console.log('job #' + job.id + ' ' + progress + '% complete.');
+      });
       queue.process(`'email${i}'`, function(job,done){
+        job.log()
         sendMail(job.data.to, done);
       })
     }
 }
-
-//function that the job has to perform.
+//
+// //function that the job has to perform.
 var sendMail = function(mail_id, done){
   console.log(mail_id);
   mailOptions.to = mail_id;
@@ -73,11 +78,27 @@ var sendMail = function(mail_id, done){
 
 
 
+//cron
+
+var task = cron.schedule('59 8 16 28 6 *', function(){
+  mailman();
+  }, false
+);
+
+
+
+var func = function(){
+  task.start();
+}
+
+
+
+
 
 //rest of the stuffs.
 app.use('/kue-api/', kue.app);
-app.use(mailman);
-
+//app.use(mailman);
+app.use(func);
 app.listen(3000);
 
 
